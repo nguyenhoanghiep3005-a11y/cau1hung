e1e9c655d2609c5fdd1819ab50aa85c4484dd57b
Nguyen Hoang Hiep <nguyenhoanghiep3005@.com>
2025-12-18 20:50:03 +0700

    updatee

--- backend/config/config.php
+++ backend/config/config.php
@@ -2,7 +2,7 @@
 return [
     'db_host'    => 'sql213.infinityfree.com',  
     'db_user'    => 'if0_40099280',            
-    'db_pass'    => 'DxyY10Dm1L',              
+    'db_pass'    => 'DxyY10DmlL',              
     'db_name'    => 'if0_40099280_sinhvien',    
     'db_charset' => 'utf8mb4'
 ];
